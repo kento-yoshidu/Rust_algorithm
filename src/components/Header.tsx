7607a2a1ce09db228b03371ed72371b86e37dc20
kento <kento@toriwatari.bird>
2021-09-26 10:18:25 +0900

    🏃 #11 Headerコンポーネントの作成

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import Link from "next/link"
+
+const Styles = require("../../styles/header.module.scss")
+
+const Header: React.VFC = () => (
+	<header className={Styles.header}>
+		<h1>NextJS microCMS Site</h1>
+		
+		<nav>
+			<ul className={Styles.mainNav}>
+				<li><Link href="https://google.com">News</Link></li>
+				<li><Link href="https://google.com">Blog</Link></li>
+				<li><Link href="https://google.com">Contact</Link></li>
+			</ul>
+		</nav>
+	</header>
+)
+
+export default Header
