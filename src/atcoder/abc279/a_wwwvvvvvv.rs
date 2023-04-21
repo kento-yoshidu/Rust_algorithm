f3824cfe89ef51f8a09aef7ef972796ff4cd1c4e
kento <kento@toriwatari.bird>
2023-04-21 19:49:18 +0900

    🈴 Update

---
+++ src/atcoder/abc279/a_wwwvvvvvv.rs
@@ -0,0 +1,25 @@
+pub fn run(str: &str) -> i32 {
+    let mut count = 0;
+
+    for c in str.chars() {
+        if c == 'w' {
+            count = count + 2;
+        } else {
+            count = count + 1;
+        }
+    }
+
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(7, run("vvwvw"));
+        assert_eq!(1, run("v"));
+        assert_eq!(12, run("wwwvvvvvv"));
+    }
+}
