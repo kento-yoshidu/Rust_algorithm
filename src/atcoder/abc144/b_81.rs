3810bd4ea564304ccd00c513f40a16ed1375db79
kento <kento@toriwatari.bird>
2023-05-25 20:02:22 +0900

    🎶 Update

---
+++ src/atcoder/abc144/b_81.rs
@@ -0,0 +1,27 @@
+pub fn run (n: i32) -> String {
+    if n > 81 {
+        return String::from("No");
+    }
+
+    for i in 0..=9 {
+        for j in 0..=9 {
+            if i * j == n {
+                return String::from("Yes");
+            }
+        }
+    }
+
+    String::from("No")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(String::from("Yes"), run(10));
+        assert_eq!(String::from("No"), run(50));
+        assert_eq!(String::from("Yes"), run(81));
+    }
+}
