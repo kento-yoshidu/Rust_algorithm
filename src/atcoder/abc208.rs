f6b640171ca60e1ab56d6887849bba1fa3159553
kento <kento@toriwatari.bird>
2023-06-02 12:26:20 +0900

    ⚡ Update

--- src/atcoder/abc208.rs
+++ src/atcoder/abc208.rs
@@ -1,1 +1,2 @@
+pub mod a_rolling_dice;
 pub mod b_factorial_yen_coin;
---
+++ src/atcoder/abc208/a_rolling_dice.rs
@@ -0,0 +1,19 @@
+pub fn run(a: i32, b: i32) -> String {
+    if a * 6 >= b {
+        String::from("Yes")
+    } else {
+        String::from("No")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(String::from("Yes"), run(2, 11));
+        assert_eq!(String::from("No"), run(2, 13));
+        assert_eq!(String::from("Yes"), run(100, 600));
+    }
+}
