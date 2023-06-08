1a10db9e2e824b738a6cda7b04f616e67897b1c1
kento <kento@toriwatari.bird>
2023-06-08 21:25:21 +0900

    👙 Update

---
+++ src/atcoder/abc184/a_determinant.rs
@@ -0,0 +1,16 @@
+#[allow(dead_code)]
+pub fn run(a: i32, b: i32, c: i32, d: i32) -> i32 {
+    a * d - b * c
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(-2, run(1, 2, 3, 4));
+        assert_eq!(1, run(0, -1, 1, 0));
+        assert_eq!(0, run(100, 100, 100, 100));
+    }
+}
