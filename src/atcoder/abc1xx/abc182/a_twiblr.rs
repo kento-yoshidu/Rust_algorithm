071edfdf62ee29cdad4647226b74cf250a6bbea4
kento <kento@toriwatari.bird>
2023-06-09 07:02:30 +0900

    🤸🏾‍♀️ Update

---
+++ src/atcoder/abc1xx/abc182/a_twiblr.rs
@@ -0,0 +1,15 @@
+#[allow(dead_code)]
+pub fn run(a: i32, b: i32) -> i32 {
+    (a * 2 + 100) - b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(200, run(200, 300));
+        assert_eq!(20100, run(10000, 0));
+    }
+}
