a348426a7c7c1408da3d016a41d3e42b3c7865d1
kento <kento@toriwatari.bird>
2023-06-09 19:54:14 +0900

    💌 Update

--- src/atcoder.rs
+++ src/atcoder.rs
@@ -16,7 +16,6 @@ pub mod abc136;
 pub mod abc137;
 pub mod abc143;
 pub mod abc144;
-pub mod abc145;
 pub mod abc146;
 pub mod abc148;
 pub mod abc151;
--- src/atcoder/abc0xx.rs
+++ src/atcoder/abc0xx.rs
@@ -2,4 +2,5 @@ pub mod abc040;
 pub mod abc041;
 pub mod abc042;
 pub mod abc043;
+pub mod abc044;
 pub mod abc083;
