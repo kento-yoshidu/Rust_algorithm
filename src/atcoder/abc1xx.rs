a0cfc4a8d856c714fef6be9add2bd965c6617831
kento <kento@toriwatari.bird>
2023-06-19 18:49:46 +0900

    🏊🏼‍♀️ Update

--- src/atcoder/abc1xx.rs
+++ src/atcoder/abc1xx.rs
@@ -38,6 +38,7 @@ pub mod abc162;
 pub mod abc164;
 pub mod abc165;
 pub mod abc169;
+pub mod abc170;
 pub mod abc173;
 pub mod abc174;
 pub mod abc176;
---
+++ src/atcoder/abc1xx/abc170/b_crane_and_turtle.rs
@@ -0,0 +1,27 @@
+#[allow(dead_code)]
+pub fn run(x: i32, y: i32) -> String {
+    //　亀をi匹とする
+    for i in 0..=x {
+        let kame = i;
+        let tsuru = x - i;
+
+        if kame*4 + tsuru*2 == y {
+            return String::from("Yes");
+        }
+    }
+
+    String::from("No")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(String::from("Yes"), run(3, 8));
+        assert_eq!(String::from("No"), run(2, 100));
+        assert_eq!(String::from("Yes"), run(25, 100));
+        assert_eq!(String::from("Yes"), run(1, 2));
+    }
+}
