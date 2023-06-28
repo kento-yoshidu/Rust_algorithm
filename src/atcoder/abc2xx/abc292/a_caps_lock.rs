3904fdc884e6a2e3ce6a74124717fd083c376828
kento <kento@toriwatari.bird>
2023-06-28 22:06:30 +0900

    🧧 Update

---
+++ src/atcoder/abc2xx/abc292/a_caps_lock.rs
@@ -0,0 +1,18 @@
+// https://atcoder.jp/contests/abc292/tasks/abc292_a
+
+#[allow(dead_code)]
+pub fn run(s: String) -> String {
+    s.to_uppercase()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(String::from("ABC"), run(String::from("abc")));
+        assert_eq!(String::from("A"), run(String::from("a")));
+        assert_eq!(String::from("ABCDEFGHJIKLNMOQPRSTVUWXYZ"), run(String::from("abcdefghjiklnmoqprstvuwxyz")));
+    }
+}
