4b78ab05217058475bdd5c54a0f10ef58504d099
kento <kento@toriwatari.bird>
2023-06-30 20:17:39 +0900

    🍱 Update

---
+++ src/atcoder/abc0xx/abc012/c_nine_nine.rs
@@ -0,0 +1,32 @@
+// https://atcoder.jp/contests/abc012/tasks/abc012_3
+
+#[allow(dead_code)]
+pub fn run(n: usize) -> Vec<String> {
+    let mut vec: Vec<(usize, usize)> = Vec::new();
+
+    let sa = 2025 - n;
+
+    for i in 1..9 {
+        for j in 1..9 {
+            if i*j == sa {
+                vec.push((i, j))
+            }
+        }
+    }
+
+    vec.iter().map(|v| {
+        format!("{} * {}", v.0, v.1)
+    }).collect::<Vec::<String>>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(vec!["2 * 6", "3 * 4", "4 * 3", "6 * 2"], run(2013));
+        assert_eq!(vec!["1 * 1"], run(2024));
+    }
+}
+
