49fb2be343557cd35174889483e8a5c1e4e64b77
kento <kento@toriwatari.bird>
2023-06-30 20:58:22 +0900

    🥣 Update

--- src/atcoder.rs
+++ src/atcoder.rs
@@ -4,6 +4,7 @@ pub mod abc2xx;
 pub mod abc3xx;
 pub mod arc0xx;
 pub mod arc1xx;
+pub mod educational_dp;
 pub mod other_contest;
 pub mod tessoku_book;
 pub mod typical90;
---
+++ src/atcoder/educational_dp/a_frog_1.rs
@@ -0,0 +1,36 @@
+// https://atcoder.jp/contests/dp/tasks/dp_a
+
+#[allow(dead_code)]
+pub fn run(n: i32, h: Vec<i32>) -> i32 {
+
+    let mut dp: Vec<i32> = Vec::new();
+
+    for _i in 0..h.len() {
+        dp.push(std::i32::MAX);
+    }
+
+    dp[0] = 0;
+
+    for i in 1..n {
+        dp[i as usize] = dp[i as usize - 1] + ((h[i as usize] - h[i as usize - 1]) as i32).abs();
+
+        if i > 1 {
+            dp[i as usize] = (dp[i as usize]).min(dp[i as usize - 2] + ((h[i as usize] - h[i as usize - 2]) as i32).abs())
+        }
+    }
+
+    dp[n as usize -1]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(8, run(7, vec![2, 9, 4, 5, 1, 6, 10]));
+        assert_eq!(30, run(4, vec![10, 30, 40, 20]));
+        assert_eq!(0, run(2, vec![10, 10]));
+        assert_eq!(40, run(6, vec![30, 10, 60, 10, 60, 50]));
+    }
+}
