5bf2fb852a91143c27abf271ffe17ed542f8f934
kento <kento@toriwatari.bird>
2023-07-02 12:53:26 +0900

    🌌 Update

---
+++ src/atcoder/abc0xx/abc012/a_swap.rs
@@ -0,0 +1,17 @@
+// https://atcoder.jp/contests/abc012/tasks/abc012_1
+
+#[allow(dead_code)]
+pub fn run(a: usize, b: usize) -> Vec<usize> {
+    vec![b, a]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(vec![2, 1], run(1, 2));
+        assert_eq!(vec![5, 5], run(5, 5));
+    }
+}
---
+++ src/atcoder/abc0xx/abc013/a_a.rs
@@ -0,0 +1,16 @@
+pub fn run(x: char) -> u8 {
+    (x as u8 - 'A' as u8) + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(1, run('A'));
+        assert_eq!(2, run('B'));
+        assert_eq!(3, run('C'));
+        assert_eq!(4, run('D'));
+    }
+}
