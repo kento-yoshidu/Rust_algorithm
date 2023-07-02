55ec6ca49afcbcaf360b1d86a7b626120a842bc2
kento <kento@toriwatari.bird>
2023-07-02 12:39:59 +0900

    🍭 Update

---
+++ src/atcoder/abc0xx/abc011/a_next_month.rs
@@ -0,0 +1,17 @@
+// https://atcoder.jp/contests/abc011/tasks/abc011_1
+
+#[allow(dead_code)]
+pub fn run(n: usize) -> usize {
+    n % 12 + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(2, run(1));
+        assert_eq!(1, run(12));
+    }
+}
