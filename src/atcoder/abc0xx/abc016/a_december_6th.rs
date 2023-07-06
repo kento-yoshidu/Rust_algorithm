417546ec8543242fbebfe83a386ff29ee26673a3
kento <kento@toriwatari.bird>
2023-07-06 21:35:58 +0900

    🍈 Update

---
+++ src/atcoder/abc0xx/abc016/a_december_6th.rs
@@ -0,0 +1,22 @@
+// https://atcoder.jp/contests/abc016/tasks/abc016_1
+
+#[allow(dead_code)]
+pub fn run(m: usize, d: usize) -> String {
+    if m % d == 0 {
+        String::from("Yes")
+    } else {
+        String::from("No")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(String::from("Yes"), run(1, 1));
+        assert_eq!(String::from("No"), run(2, 29));
+        assert_eq!(String::from("Yes"), run(12, 6));
+    }
+}
