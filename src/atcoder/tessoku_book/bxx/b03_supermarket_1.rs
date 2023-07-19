0eebe3b22dd48fa22bce9a7666659ad2cfe22fbd
kento <kento@toriwatari.bird>
2023-07-19 20:01:30 +0900

    😜 Update

--- src/atcoder/tessoku_book/bxx/b03_supermarket_1.rs
+++ src/atcoder/tessoku_book/bxx/b03_supermarket_1.rs
@@ -5,8 +5,6 @@ pub fn run(n: usize, vec: Vec<usize>) -> String {
     for i in 0..n {
         for j in i+1..n {
             for k in j+1..n {
-                println!("i={}, j={}, k={}", vec[i], vec[j], vec[k]);
-
                 if vec[i] + vec[j] + vec[k] == 1000 {
                     return  String::from("Yes");
                 }
@@ -25,6 +23,5 @@ mod tests {
     fn test() {
         assert_eq!(String::from("Yes"), run(5, vec![100, 250, 350, 400, 600]));
         assert_eq!(String::from("No"), run(10, vec![50, 150, 250, 350, 450, 550, 650, 750, 850, 950]))
-
     }
 }
