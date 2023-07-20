ba8675e4e4256dce45caaf0c8ed3d5cec1c9dbf5
kento <kento@toriwatari.bird>
2023-07-20 20:42:04 +0900

    🌃 Update

---
+++ src/atcoder/abc2xx/abc287/a_majority.rs
@@ -0,0 +1,33 @@
+// https://atcoder.jp/contests/abc287/tasks/abc287_a
+
+#[allow(dead_code)]
+pub fn run(_n: usize, vec: Vec<&str>) -> String {
+    let mut f = 0;
+    let mut a = 0;
+
+    for v in vec.iter() {
+        if *v == "For" {
+            f += 1;
+        } else {
+            a += 1;
+        }
+    }
+
+    if f > a {
+        String::from("Yes")
+    } else {
+        String::from("No")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(String::from("Yes"), run(7, vec!["For", "Against", "For"]));
+        assert_eq!(String::from("No"), run(5, vec!["Against", "Against", "For", "Against", "For"]));
+        assert_eq!(String::from("Yes"), run(1, vec!["For"]));
+    }
+}
