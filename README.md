c5200904d39023cae2603171d2ce4272ae87cb89
kento <kento@toriwatari.bird>
2023-07-30 11:26:45 +0900

    🚿 Update

---
+++ README.md
@@ -0,0 +1,29 @@
+# Rust_algorithm
+
+## AtCoder Template
+
+```rust
+use proconio::input;
+
+fn main() {
+    input! {
+        a: i32,
+        p: i32
+    }
+
+    let all_p = a * 3 +p;
+
+    println!("{}", all_p / 2);
+}
+```
+
+## Todo
+
+abc147 b
+abc294 a
+abc221 c
+
+## Refactoring
+
+abc197 b
+tessoku bc
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,8 @@
-/* AtCoder Template
-use proconio::input;
-
-fn main() {
-    input! {
-        a: i32,
-        p: i32
-    }
-    let all_p = a * 3 +p;
-    println!("{}", all_p / 2);
-}
-*/
-
-// use itertools::Itertools;
-
+#![allow(dead_code)]
 mod atcoder;
 mod nlp_100_knock;
 mod aoj;
 
-#[allow(dead_code)]
 fn main() {
     println!("Hello, world!");
 }
-
-// Todo
-// abc147 b
-// abc294 a
-// abc221 c
-
-// Refactoring
-// abc197 b
-// tessoku bc
