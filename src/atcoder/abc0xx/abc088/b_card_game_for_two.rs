e84169f2264d4ce6bf35caaed6c1c4214d6aad9d
kento <kento@toriwatari.bird>
2023-09-22 20:11:50 +0900

    🧏‍♀️ Update

--- src/atcoder/abc0xx/abc088/b_card_game_for_two.rs
+++ src/atcoder/abc0xx/abc088/b_card_game_for_two.rs
@@ -1,6 +1,5 @@
 // https://atcoder.jp/contests/abc088/tasks/abc088_b
 
-#[allow(dead_code)]
 pub fn run(_n: i32, vec: &mut Vec<i32>) -> i32 {
     vec.sort();
 
@@ -17,6 +16,22 @@ pub fn run(_n: i32, vec: &mut Vec<i32>) -> i32 {
     ans
 }
 
+pub fn run2(_n: isize, vec: &mut Vec<isize>) -> isize {
+    vec.sort();
+
+    vec
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(i, num)| {
+            if i % 2 == 0 {
+                *num
+            } else {
+                -num
+            }
+        }).sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -26,5 +41,12 @@ mod tests {
         assert_eq!(2, run(2, &mut vec![3, 1]));
         assert_eq!(5, run(2, &mut vec![2, 7, 4]));
         assert_eq!(18, run(2, &mut vec![20, 18, 2, 18]));
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(2, run2(2, &mut vec![3, 1]));
+        assert_eq!(5, run2(2, &mut vec![2, 7, 4]));
+        assert_eq!(18, run2(2, &mut vec![20, 18, 2, 18]));
     }
 }
