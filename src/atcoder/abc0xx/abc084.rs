912b1fd9b6f2c8013ac282be52a1c9de55cf83be
kento <kento@toriwatari.bird>
2023-09-29 19:55:48 +0900

    🏍 Update

--- src/atcoder/abc0xx/abc084.rs
+++ src/atcoder/abc0xx/abc084.rs
@@ -1,1 +1,2 @@
+pub mod a_new_year;
 pub mod b_postal_code;
---
+++ src/atcoder/abc0xx/abc084/a_new_year.rs
@@ -0,0 +1,16 @@
+// https://atcoder.jp/contests/abc084/tasks/abc084_a
+
+fn run(m: usize) -> usize {
+	(24 - m ) + 24
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test() {
+		assert_eq!(27, run(21));
+		assert_eq!(36, run(12));
+	}
+}
