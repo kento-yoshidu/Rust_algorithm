b85cddc5a197a918504d57801f3f024bd74e55ca
kento <kento@toriwatari.bird>
2023-10-13 20:44:17 +0900

    ☠ Update

--- src/atcoder/other_contest/tenka1_2012_qual_b/a_sonshi.rs
+++ src/atcoder/other_contest/tenka1_2012_qual_b/a_sonshi.rs
@@ -1,6 +1,5 @@
 // https://atcoder.jp/contests/tenka1-2012-qualb/tasks/tenka1_2012_5
 
-#[allow(unused)]
 pub fn run(a: usize, b: usize, c: usize) -> Vec<usize> {
     let mut ans: Vec<usize> = Vec::new();
 
@@ -13,6 +12,12 @@ pub fn run(a: usize, b: usize, c: usize) -> Vec<usize> {
     ans
 }
 
+pub fn run2(a: usize, b: usize, c: usize) -> Vec<usize> {
+    (1..=127).filter(|i| {
+        i % 3 == a && i % 5 == b && i % 7 == c
+    }).collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,5 +27,12 @@ mod tests {
         assert_eq!(vec![23], run(2, 3, 2));
         assert_eq!(vec![1, 106], run(1, 1, 1));
         assert_eq!(vec![104], run(2, 4, 6));
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(vec![23], run2(2, 3, 2));
+        assert_eq!(vec![1, 106], run2(1, 1, 1));
+        assert_eq!(vec![104], run2(2, 4, 6));
     }
 }
