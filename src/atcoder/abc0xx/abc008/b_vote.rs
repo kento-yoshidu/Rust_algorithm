722abbb4113ae4add1decabfa346e0567d91b42e
kento <kento@toriwatari.bird>
2023-10-17 20:26:50 +0900

    🦧 Update

--- src/atcoder/abc0xx/abc008/b_vote.rs
+++ src/atcoder/abc0xx/abc008/b_vote.rs
@@ -20,6 +20,6 @@ mod tests {
     fn test() {
         assert_eq!(String::from("taro"), run(4, vec!["taro", "jiro", "taro", "saburo"]));
         assert_eq!(String::from("takahashikun"), run(1, vec!["takahashikun"]));
-        assert_eq!(String::from("b"), run(9, vec!["a", "b", "c", "c", "b", "c", "b", "d", "e"]));
+        assert_eq!(String::from("b"), run(9, vec!["a", "b", "c", "c", "b", "c", "b", "d", "e", "b"]));
     }
 }
--- src/atcoder/abc0xx/abc052.rs
+++ src/atcoder/abc0xx/abc052.rs
@@ -1,1 +1,2 @@
 pub mod a_two_rectangles;
+pub mod b_increment_decrement;
