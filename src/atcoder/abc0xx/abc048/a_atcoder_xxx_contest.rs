2d5eea4bf00f1d227b6d72c3c3ee125773372bcb
kento <kento@toriwatari.bird>
2023-10-30 20:20:28 +0900

    🐊 Update

--- src/atcoder/abc0xx/abc048/a_atcoder_xxx_contest.rs
+++ src/atcoder/abc0xx/abc048/a_atcoder_xxx_contest.rs
@@ -12,6 +12,16 @@ pub fn run(a: String, b: String, c: String) -> String {
     ans
 }
 
+pub fn run2(a: &str, b: &str, c: &str) -> String {
+    let vec = vec![a, b, c];
+
+    vec.iter()
+        .map(|v| {
+            v.chars().nth(0).unwrap()
+        })
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -21,5 +31,12 @@ mod tests {
         assert_eq!(String::from("ABC"), run(String::from("AtCoder"), String::from("Beginner"), String::from("Contest")));
         assert_eq!(String::from("ASC"), run(String::from("AtCoder"), String::from("Snuke"), String::from("Contest")));
         assert_eq!(String::from("AXC"), run(String::from("AtCoder"), String::from("X"), String::from("Contest")));
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(String::from("ABC"), run2("AtCoder", "Beginner", "Contest"));
+        assert_eq!(String::from("ASC"), run2("AtCoder", "Snuke", "Contest"));
+        assert_eq!(String::from("AXC"), run2("AtCoder", "X", "Contest"));
     }
 }
