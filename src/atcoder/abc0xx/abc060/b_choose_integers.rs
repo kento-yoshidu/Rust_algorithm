9dff39d2217fc268540b812b67e7d1db899616f2
kento <kento@toriwatari.bird>
2023-10-31 19:46:35 +0900

    🎷 Update

---
+++ src/atcoder/abc0xx/abc060/b_choose_integers.rs
@@ -0,0 +1,27 @@
+// https://atcoder.jp/contests/abc060/tasks/abc060_b
+
+pub fn run( a: usize, b: usize, c: usize) -> String {
+    if (a..=a*b)
+        .step_by(a)
+        .any(|num| {
+        num % b == c
+    }) {
+        String::from("YES")
+    } else {
+        String::from("NO")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(String::from("YES"), run(7, 5, 1));
+        assert_eq!(String::from("NO"), run(2, 2, 1));
+        assert_eq!(String::from("YES"), run(1, 100, 97));
+        assert_eq!(String::from("YES"), run(40, 98, 58));
+        assert_eq!(String::from("NO"), run(77, 42, 36));
+    }
+}
