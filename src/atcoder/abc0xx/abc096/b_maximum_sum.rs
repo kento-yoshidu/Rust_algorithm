e9e930c4e1a0a4a1d67804e035526e93d0d4fa97
kento <kento@toriwatari.bird>
2023-11-03 23:14:29 +0900

    🦒 Update

--- src/atcoder/abc0xx/abc096/b_maximum_sum.rs
+++ src/atcoder/abc0xx/abc096/b_maximum_sum.rs
@@ -6,6 +6,14 @@ pub fn run(v: Vec<usize>, k: usize) -> usize {
     sum + v.iter().max().unwrap() * (2 * k -1)
 }
 
+pub fn run2(v: Vec<usize>, k: usize) -> usize {
+    let max = v.iter().max().unwrap();
+
+    let rest: usize = v.iter().filter(|num| *num != max).sum();
+
+    max * (2 * k) + rest
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -14,5 +22,11 @@ mod tests {
     fn test() {
         assert_eq!(30, run(vec![5, 3, 11], 1));
         assert_eq!(22, run(vec![3, 3, 4], 2));
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(30, run2(vec![5, 3, 11], 1));
+        assert_eq!(22, run2(vec![3, 3, 4], 2));
     }
 }
