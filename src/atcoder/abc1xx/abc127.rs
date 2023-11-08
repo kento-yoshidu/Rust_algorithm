6566e0d63ae2b5c9a11edfc95d3794cda1b463cf
kento <kento@toriwatari.bird>
2023-11-08 20:10:39 +0900

    🏏 Update

--- src/atcoder/abc1xx/abc127.rs
+++ src/atcoder/abc1xx/abc127.rs
@@ -1,1 +1,2 @@
 pub mod a_ferris_wheel;
+pub mod b_algae;
--- src/atcoder/abc1xx/abc169/b_multiplication_2.rs
+++ src/atcoder/abc1xx/abc169/b_multiplication_2.rs
@@ -1,14 +1,14 @@
 // https://atcoder.jp/contests/abc169/tasks/abc169_b
 
-pub fn run(_n: usize, a: Vec<isize>) -> isize {
+pub fn run(_n: usize, a: Vec<i128>) -> i128 {
     if a.contains(&0) {
         return 0
     }
 
-    let mut ans: isize = 1;
+    let mut ans: i128 = 1;
 
     for num in a {
-        if ans * num > 10_isize.pow(18_u32) {
+        if ans * num > 10_i128.pow(18_u32) {
             ans = -1;
             break;
         } else {
@@ -16,13 +16,13 @@ pub fn run(_n: usize, a: Vec<isize>) -> isize {
         }
     }
 
-    ans as isize
+    ans as i128
 }
 
-pub fn run2(_n: usize, a: Vec<isize>) -> isize {
+pub fn run2(_n: usize, a: Vec<i128>) -> i128 {
     a.iter()
         .fold(1, |state, num| {
-            if state * num > 10_usize.pow(18_u32) as isize {
+            if state * num > 10_usize.pow(18_u32) as i128 {
                 -1
             } else {
                 state * num
@@ -39,5 +39,12 @@ mod tests {
         assert_eq!(1000000000000000000, run(2, vec![1000000000, 1000000000]));
         assert_eq!(-1, run(3, vec![101, 9901, 999999000001]));
         assert_eq!(0, run(31, vec![4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6, 4, 3, 3, 8, 3, 2, 7, 9, 5, 0]));
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(1000000000000000000, run2(2, vec![1000000000, 1000000000]));
+        assert_eq!(-1, run2(3, vec![101, 9901, 999999000001]));
+        assert_eq!(0, run2(31, vec![4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6, 4, 3, 3, 8, 3, 2, 7, 9, 5, 0]));
     }
 }
