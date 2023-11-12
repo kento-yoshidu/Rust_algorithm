623d6b74220682f3e185ba4bf9d309a6485f7e55
kento <kento@toriwatari.bird>
2023-11-12 20:57:28 +0900

    🆕 Update

--- src/atcoder/abc1xx/abc121/b_can_you_solve_this.rs
+++ src/atcoder/abc1xx/abc121/b_can_you_solve_this.rs
@@ -18,6 +18,21 @@ pub fn run(_n: i32, _m: i32, c: i32, b: Vec<i32>, vec: Vec<Vec<i32>>) -> i32 {
     ans
 }
 
+pub fn run2(_n: i32, _m: i32, c: i32, n: Vec<i32>, vec: Vec<Vec<i32>>) -> usize {
+    vec.iter()
+        .filter(|v| {
+            let total: i32 = v.iter()
+                .enumerate()
+                .map(|(i, num)| {
+                    num * n[i]
+                })
+                .sum();
+
+            total + c > 0
+        })
+        .count()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -27,5 +42,12 @@ mod tests {
         assert_eq!(1, run(2, 3, -10, vec![1, 2, 3], vec![vec![3, 2, 1], vec![1, 2, 2]]));
         assert_eq!(2, run(5, 2, -4, vec![-2, 5], vec![vec![100, 41], vec![100, 40], vec![-3, 0], vec![-6, -2], vec![18, -13]]));
         assert_eq!(0, run(3, 3, 0, vec![100, -100, 0], vec![vec![0, 100, 100], vec![100, 100, 100], vec![-100, 100, 100]]))
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(1, run2(2, 3, -10, vec![1, 2, 3], vec![vec![3, 2, 1], vec![1, 2, 2]]));
+        assert_eq!(2, run2(5, 2, -4, vec![-2, 5], vec![vec![100, 41], vec![100, 40], vec![-3, 0], vec![-6, -2], vec![18, -13]]));
+        assert_eq!(0, run2(3, 3, 0, vec![100, -100, 0], vec![vec![0, 100, 100], vec![100, 100, 100], vec![-100, 100, 100]]))
     }
 }
