bb4cf9f9ee4466b51cca0110c27b528cc2f29d86
kento <kento@toriwatari.bird>
2023-11-15 21:17:32 +0900

    🐉 Update

--- src/atcoder/abc1xx/abc159/b_string_palindrome.rs
+++ src/atcoder/abc1xx/abc159/b_string_palindrome.rs
@@ -1,13 +1,15 @@
 // https://atcoder.jp/contests/abc159/tasks/abc159_b
 
 fn check(s: &str) -> bool {
+	println!("{}", s);
+
 	s.chars().eq(s.chars().rev())
 }
 
 fn run(s: &str) -> String {
 	let n = s.len();
 
-	if check(&s[0..(n-1)/2]) && check(&s[((n+3)/2)-1..]) {
+	if check(&s) && check(&s[0..(n-1)/2]) && check(&s[((n+3)/2)-1..]) {
 		String::from("Yes")
 	} else {
 		String::from("No")
@@ -23,5 +25,6 @@ mod tests {
 		assert_eq!(String::from("Yes"), run("akasaka"));
 		assert_eq!(String::from("No"), run("level"));
 		assert_eq!(String::from("No"), run("atcoder"));
+		assert_eq!(String::from("No"), run("igyojknviataivnkjoygizigyojknviaaaivnkjoygi"));
 	}
 }
