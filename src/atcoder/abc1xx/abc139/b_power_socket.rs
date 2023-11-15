06fefab118f1999dced5aa919a442d4134c4102d
kento <kento@toriwatari.bird>
2023-11-15 20:33:44 +0900

    ❎ Update

--- src/atcoder/abc1xx/abc139/b_power_socket.rs
+++ src/atcoder/abc1xx/abc139/b_power_socket.rs
@@ -1,10 +1,10 @@
 // https://atcoder.jp/contests/abc139/tasks/abc139_b
 
 fn run(a: usize, b: usize) -> usize {
-    if a == b {
-        1
+    if (b - 1) % (a - 1) == 0 {
+        (b - 1) / (a - 1)
     } else {
-        b / a + 1
+        (b - 1) / (a - 1) + 1
     }
 }
 
@@ -17,5 +17,6 @@ mod tests {
         assert_eq!(3, run(4, 10));
         assert_eq!(2, run(8, 9));
         assert_eq!(1, run(8, 8));
+        assert_eq!(4, run(4, 12));
     }
 }
