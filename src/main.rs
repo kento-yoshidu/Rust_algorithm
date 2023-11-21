6082c2cfd6e1c99cb726f2a8f92c91e733463433
kento <kento@toriwatari.bird>
2023-11-21 20:27:16 +0900

    🔂 Update

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,4 @@ mod yukicoder;
 
 fn main() {
     println!("Hello World");
-    println!("{:?}", atcoder::abc1xx::abc173::b_judge_status_summary::run2(6, vec!["AC", "TLE", "AC", "AC", "WA", "TLE"]));
 }
