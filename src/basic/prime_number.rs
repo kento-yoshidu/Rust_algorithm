8041b5d4622cefa6e51e4296d763af50fbc35373
kento <kento@toriwatari.bird>
2023-11-26 11:45:22 +0900

    🍁 Update

--- src/basic/prime_number.rs
+++ src/basic/prime_number.rs
@@ -1,1 +1,2 @@
-pub mod prime_number;
+pub mod p01_prime_number;
+pub mod p02_sieve_of_eratosthenes;
--- src/basic/prime_number/p01_prime_number.rs
+++ src/basic/prime_number/p01_prime_number.rs
@@ -17,18 +17,3 @@ mod tests {
         assert_eq!(vec![true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true], is_prime_a(vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]));
     }
 }
-
-/*
-pub fn next_prime_number(n: usize) -> usize {
-    let res = (n+2..n*2).step_by(2).find(|num| {
-        (2..*num/2).all(|i| {
-            *num % i != 0
-        })
-    });
-
-    println!("{:?}", res);
-
-    10
-}
-*/
-
