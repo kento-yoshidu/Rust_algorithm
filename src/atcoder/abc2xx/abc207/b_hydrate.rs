a248847dcfe5ac4b36011d57d9a120929815c001
kento <kento@toriwatari.bird>
2023-11-29 19:52:01 +0900

    🧨 Update

---
+++ src/atcoder/abc2xx/abc207/b_hydrate.rs
@@ -0,0 +1,15 @@
+// https://atcoder.jp/contests/abc207/tasks/abc207_b
+
+fn calc(count: isize, state: (usize, usize), d: usize) -> isize {
+
+}
+
+pub fn run(a: usize, b: usize, c: usize, d: usize) -> isize {
+    if b < c {
+        return -1
+    } else if a * d < c {
+        return 0
+    } else {
+        
+    }
+}
