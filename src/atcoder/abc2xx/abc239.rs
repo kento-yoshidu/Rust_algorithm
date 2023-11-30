b8e4e53111b7e2db8949147a6128a5f38cd571fa
kento <kento@toriwatari.bird>
2023-11-30 21:28:46 +0900

    💐 Update

--- src/atcoder/abc2xx/abc239.rs
+++ src/atcoder/abc2xx/abc239.rs
@@ -1,1 +1,2 @@
 pub mod a_horizon;
+pub mod b_integer_division;
---
+++ src/atcoder/abc2xx/abc239/b_integer_division.rs
@@ -0,0 +1,9 @@
+// https://atcoder.jp/contests/abc239/tasks/abc239_b
+
+pub fn run(x: i128) -> u128 {
+    let c = x as f64 / 10.0;
+
+    println!("{}", c.floor() as i128);
+
+    10
+}
