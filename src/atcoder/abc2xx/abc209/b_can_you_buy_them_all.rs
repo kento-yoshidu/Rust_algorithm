188f113be9707dda8f17c71a8018c63a8f0b69ff
kento <kento@toriwatari.bird>
2023-12-05 20:50:17 +0900

    🗝 Update

--- src/atcoder/abc2xx/abc209/b_can_you_buy_them_all.rs
+++ src/atcoder/abc2xx/abc209/b_can_you_buy_them_all.rs
@@ -5,9 +5,9 @@ pub fn run(_n: usize, x: usize, a: Vec<usize>) -> String {
         .enumerate()
         .map(|(i, &num)| {
             if i % 2 == 0 {
-                num - 1
-            } else {
                 num
+            } else {
+                num - 1
             }
         })
         .sum();
@@ -28,5 +28,6 @@ mod tests {
         assert_eq!(String::from("Yes"), run(2, 3, vec![1, 3]));
         assert_eq!(String::from("No"), run(4, 10, vec![3, 3, 4, 4]));
         assert_eq!(String::from("Yes"), run(8, 30, vec![3, 1, 4, 1, 5, 9, 2, 6]));
+        assert_eq!(String::from("No"), run(15, 775, vec![88, 57, 44, 92, 28, 66, 60, 37, 33, 52, 38, 29, 76, 8, 75]));
     }
 }
