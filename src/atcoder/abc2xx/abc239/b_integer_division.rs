6d858d6f2a10ad85665410233689b86292dd15ac
kento <kento@toriwatari.bird>
2024-01-11 22:43:39 +0900

    🙉 Update

--- src/atcoder/abc2xx/abc239/b_integer_division.rs
+++ src/atcoder/abc2xx/abc239/b_integer_division.rs
@@ -1,9 +1,27 @@
 // https://atcoder.jp/contests/abc239/tasks/abc239_b
 
-pub fn run(x: i128) -> u128 {
-    let c = x as f64 / 10.0;
+pub fn run(x: isize) -> isize {
+    if x > 0 {
+        x / 10
+    } else {
+        if x % 10 == 0 {
+            x / 10
+        } else {
+            x / 10 -1
+        }
+    }
+}
 
-    println!("{}", c.floor() as i128);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    10
+    #[test]
+    fn test() {
+        assert_eq!(4, run(47));
+        assert_eq!(-3, run(-24));
+        assert_eq!(5, run(50));
+        assert_eq!(-3, run(-30));
+        assert_eq!(98765432198765432, run(987654321987654321));
+    }
 }
