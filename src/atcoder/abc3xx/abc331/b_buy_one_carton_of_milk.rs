37de688c05a4fc23e2baa161b84ce0bab9abdad8
kento <kento@toriwatari.bird>
2024-01-12 20:26:00 +0900

    💈 Update

--- src/atcoder/abc3xx/abc331/b_buy_one_carton_of_milk.rs
+++ src/atcoder/abc3xx/abc331/b_buy_one_carton_of_milk.rs
@@ -1,5 +1,7 @@
 // https://atcoder.jp/contests/abc331/tasks/abc331_b
 
+use itertools::iproduct;
+
 pub fn run(n: usize, s: usize, m: usize, l: usize) -> usize {
     let mut ans = std::usize::MAX;
 
@@ -24,6 +26,18 @@ pub fn run(n: usize, s: usize, m: usize, l: usize) -> usize {
     ans
 }
 
+pub fn run2(n: usize, s: usize, m: usize, l: usize) -> usize {
+    let mut ans = std::usize::MAX;
+
+    for (i, j, k) in iproduct!(0..=100, 0..=100, 0..=100) {
+        if i*6 + j*8 + k*12 >= n {
+            ans = ans.min(i*s + j*m + k*l);
+        }
+    }
+
+    ans
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -33,5 +47,12 @@ mod tests {
         assert_eq!(300, run(16, 120,  150, 200));
         assert_eq!(10, run(10, 100, 50, 10));
         assert_eq!(10000, run(99, 600, 800, 1200));
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(300, run2(16, 120,  150, 200));
+        assert_eq!(10, run2(10, 100, 50, 10));
+        assert_eq!(10000, run2(99, 600, 800, 1200));
     }
 }
