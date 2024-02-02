cf60780b40fef8b5982e05aaaa6ae37d512bc237
kento <kento@toriwatari.bird>
2024-02-02 20:37:32 +0900

    🎂 Update

---
+++ src/atcoder/abc2xx/abc238/b_pizza.rs
@@ -0,0 +1,43 @@
+// https://atcoder.jp/contests/abc238/tasks/abc238_b
+
+pub fn run(_n: usize, a: Vec<isize>) -> isize {
+    let mut vec = vec![0];
+
+    let mut current = 0;
+
+    for num in a {
+        current += num;
+        current %= 360;
+        vec.push(current);
+    }
+
+    vec.push(360);
+    vec.sort();
+
+    vec.windows(2)
+        .map(|arr| {
+            arr[1] - arr[0]
+        })
+        .max()
+        .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(usize,  Vec<isize>, isize);
+
+    #[test]
+    fn test() {
+        let tests = [
+            TestCase(4, vec![90, 180, 45, 195], 120),
+            TestCase(1, vec![1], 359),
+            TestCase(10, vec![215, 137, 320, 339, 341, 41, 44, 18, 241, 149], 170),
+        ];
+
+        for TestCase(n, a, expected) in tests {
+            assert_eq!(run(n, a), expected);
+        }
+    }
+}
