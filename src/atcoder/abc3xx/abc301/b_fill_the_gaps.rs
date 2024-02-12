1ac96a9dce63d48c417079b1495d727272ab5cae
kento <kento@toriwatari.bird>
2024-02-12 20:42:56 +0900

    🥶 Update

--- src/atcoder/abc3xx/abc301/b_fill_the_gaps.rs
+++ src/atcoder/abc3xx/abc301/b_fill_the_gaps.rs
@@ -1,29 +1,39 @@
 // https://atcoder.jp/contests/abc301/tasks/abc301_b
 
-/*
-pub fn run(n: usize, a: Vec<i32>) -> Vec<i32> {
-    let mut ans = vec![0];
+pub fn run(n: usize, a: Vec<isize>) -> Vec<isize> {
+    let mut ans = Vec::new();
 
-    a.iter()
-        .skip(1)
-        .fold(vec![a[0]], |mut stack, num| {
-            let last = stack.iter().last().unwrap();
-
-            if (last - num).abs() == 1 {
-                stack.push(*num);
-                stack
-            } else {
-                if last < num {
-                    stack.push(*num);
-
-                    for i in (*last+1..*num).rev() {
-                        stack.push(i);
-                    }
-                    stack
-                } else {
-                    stack
-                }
+    for i in 0..n-1 {
+        if a[i] > a[i+1] {
+            for i in (a[i+1]..=a[i]).rev() {
+                ans.push(i);
             }
-        })
+        } else {
+            for i in a[i]..=a[i+1] {
+                ans.push(i)
+            }
+        }
+    }
+
+    ans.dedup();
+
+    ans
 }
-*/
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(usize, Vec<isize>, Vec<isize>);
+    #[test]
+    fn test() {
+        let tests = [
+            TestCase(4, vec![2, 5, 1, 2], vec![2, 3, 4, 5, 4, 3, 2, 1, 2]),
+            TestCase(6, vec![3, 4, 5, 6, 5, 4], vec![3, 4, 5, 6, 5, 4]),
+        ];
+
+        for TestCase(n, a, expected) in tests {
+            assert_eq!(run(n, a), expected);
+        }
+    }
+}
