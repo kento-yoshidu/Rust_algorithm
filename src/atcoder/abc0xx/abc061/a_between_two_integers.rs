3639db130dd3a7f9f5bca20e5530f0db63e6bfab
kento <kento@toriwatari.bird>
2024-03-06 19:55:10 +0900

    🎰 Update

--- src/atcoder/abc0xx/abc061/a_between_two_integers.rs
+++ src/atcoder/abc0xx/abc061/a_between_two_integers.rs
@@ -1,10 +1,10 @@
 // https://atcoder.jp/contests/abc060/tasks/abc060_a
 
-pub fn run(a: i32, b: i32, c: i32) -> String {
+pub fn run(a: i32, b: i32, c: i32) -> &'static str {
     if a <= c && c <= b {
-        String::from("Yes")
+        "Yes"
     } else {
-        String::from("No")
+        "No"
     }
 }
 
@@ -12,10 +12,19 @@ pub fn run(a: i32, b: i32, c: i32) -> String {
 mod tests {
     use super::*;
 
+    struct TestCase(i32, i32, i32, &'static str);
+
     #[test]
     fn test() {
-        assert_eq!(String::from("Yes"), run(1, 3, 2));
-        assert_eq!(String::from("No"), run(6, 5, 4));
-        assert_eq!(String::from("Yes"), run(2, 2, 2));
+        let tests = [
+            TestCase(1, 3, 2, "Yes"),
+            TestCase(1, 3, 2, "Yes"),
+            TestCase(6, 5, 4, "No"),
+            TestCase(2, 2, 2, "Yes"),
+        ];
+
+        for TestCase(a, b, c, expected) in tests {
+            assert_eq!(run(a, b, c), expected);
+        }
     }
 }
--- src/atcoder/abc1xx/abc153.rs
+++ src/atcoder/abc1xx/abc153.rs
@@ -1,3 +1,4 @@
 pub mod a_serval_vs_monster;
 pub mod b_common_raccoon_vs_monster;
 pub mod c_fennec_vs_monster;
+pub mod d_caracal_vs_monster;
