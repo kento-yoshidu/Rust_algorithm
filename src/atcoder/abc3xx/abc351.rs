75ba47479188291a07fb811a68be7964fbff0fda
kento <kento@toriwatari.bird>
2024-04-28 16:56:27 +0900

    🧁 Update

--- src/atcoder/abc3xx/abc351.rs
+++ src/atcoder/abc3xx/abc351.rs
@@ -1,2 +1,3 @@
 pub mod a_the_bottom_of_the_ninth;
 pub mod b_spot_the_difference;
+pub mod c_merge_the_balls;
---
+++ src/atcoder/abc3xx/abc351/c_merge_the_balls.rs
@@ -0,0 +1,46 @@
+// https://atcoder.jp/contests/abc351/tasks/abc351_c
+
+pub fn run(_n: usize, a: Vec<usize>) -> usize {
+    let vec = a.clone();
+
+    vec.into_iter()
+        .fold(Vec::new(), |mut stack, num| {
+            stack.push(num);
+            println!("{:?}", stack);
+
+            loop {
+                println!("\t{:?}", stack);
+                let len = stack.len();
+
+                if len > 1 && stack[len-2] == stack[len-1] {
+                    let i = stack[len-1];
+
+                    stack.truncate(len-2);
+
+                    stack.push(i+1);
+                } else {
+                    break stack;
+                }
+            }
+        })
+        .len()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(usize, Vec<usize>, usize);
+
+    #[test]
+    fn test() {
+        let tests = [
+            TestCase(7 , vec![2, 1, 1, 3, 5, 3, 3], 3),
+            TestCase(5, vec![0, 0, 0, 1, 2], 4),
+        ];
+
+        for TestCase(n, a, expected) in tests {
+            assert_eq!(run(n, a), expected);
+        }
+    }
+}
