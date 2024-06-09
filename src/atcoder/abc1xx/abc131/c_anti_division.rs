ef8e0c5a729199f79c97c48e148afa9145b1eb9d
kento <kento@toriwatari.bird>
2024-06-09 21:15:14 +0900

    🍃 Update

---
+++ src/atcoder/abc1xx/abc131/c_anti_division.rs
@@ -0,0 +1,45 @@
+// https://atcoder.jp/contests/abc131/tasks/abc131_c
+
+fn gcd(c: usize, d: usize) -> usize {
+    if d == 0 {
+        c
+    } else {
+        gcd(d, c % d)
+    }
+}
+
+fn lcm(c: usize, d: usize) -> usize {
+    c / gcd(c, d) * d
+}
+
+fn calc(num: usize, c: usize, d: usize) -> usize {
+    let c_count = num / c;
+    let d_count = num / d;
+    let l = lcm(c, d);
+
+    num - c_count - d_count + num / l
+}
+
+pub fn run(a: usize, b: usize, c: usize, d: usize) -> usize {
+    calc(b, c, d) - calc(a-1, c, d)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(usize, usize, usize, usize, usize);
+
+    #[test]
+    fn test() {
+        let tests = [
+            TestCase(4, 9, 2, 3, 2),
+            TestCase(10, 40, 6, 8, 23),
+            TestCase(314159265358979323, 846264338327950288, 419716939, 937510582, 532105071133627368),
+        ];
+
+        for TestCase(a, b, c, d, expected) in tests {
+            assert_eq!(run(a, b, c, d), expected);
+        }
+    }
+}
