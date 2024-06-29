fd0cb14618282399240882442f808edf2ac80e3c
kento <kento@toriwatari.bird>
2024-06-29 13:03:45 +0900

    😦 Update

--- src/atcoder/abc2xx/abc261/a_intersection.rs
+++ src/atcoder/abc2xx/abc261/a_intersection.rs
@@ -1,17 +1,27 @@
-use std::cmp;
+// https://atcoder.jp/contests/abc261/tasks/abc261_a
 
-pub fn run(l1: i8, r1: i8, l2: i8, r2: i8) -> i8 {
-    cmp::max(0, cmp::min(r1, r2) - cmp::max(l1, l2))
+use std::cmp::{min, max};
+
+fn run(l1: isize, r1: isize, l2: isize, r2: isize) -> isize {
+    max(0, min(r1, r2) - max(l1, l2))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct TestCase(isize, isize, isize, isize, isize);
+
     #[test]
     fn test() {
-        assert_eq!(run(0, 3, 1, 5), 2);
-        assert_eq!(run(0, 1, 4, 5), 0);
-        assert_eq!(run(0, 3, 3, 7), 0);
+        let tests = [
+            TestCase(0, 3, 1, 5, 2),
+            TestCase(0, 1, 4, 5, 0),
+            TestCase(0, 3, 3, 7, 0),
+        ];
+
+        for TestCase(l1, r1, l2, r2, expected) in tests {
+            assert_eq!(run(l1, r1, l2, r2), expected);
+        }
     }
 }
