cfcc2f1495a1c8fed891be3da6c2ae5dcb924e98
kento <kento@toriwatari.bird>
2024-07-11 20:36:11 +0900

    🧍‍♂️ Update

---
+++ src/atcoder/arc0xx/arc018/a_bmi.rs
@@ -0,0 +1,24 @@
+// https://atcoder.jp/contests/arc018/tasks/arc018_1
+
+fn run(height: f64, bmi: f64) -> f64 {
+    bmi * height.powf(2.0) / 10000.0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(f64, f64, f64);
+
+    #[test]
+    fn test() {
+        let tests = [
+            TestCase(160.0, 23.5, 60.16),
+            TestCase(199.9, 11.1, 44.356),
+        ];
+
+        for TestCase(height, bmi, expected) in tests {
+            assert_eq!(run(height, bmi), expected);
+        }
+    }
+}
