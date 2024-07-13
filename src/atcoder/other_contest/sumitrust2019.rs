9d3dba228b248fd4dc04a8cef6cf7f02baf4b603
kento <kento@toriwatari.bird>
2024-07-13 11:47:32 +0900

    🌆 Update

--- src/atcoder/other_contest/sumitrust2019.rs
+++ src/atcoder/other_contest/sumitrust2019.rs
@@ -1,2 +1,3 @@
+pub mod a_november_30;
 pub mod b_tax_rate;
 pub mod c_100_to_105;
---
+++ src/atcoder/other_contest/sumitrust2019/a_november_30.rs
@@ -0,0 +1,28 @@
+// https://atcoder.jp/contests/sumitrust2019/tasks/sumitb2019_a
+
+fn run(m1: usize, d1: usize, m2: usize, d2: usize) -> usize {
+    if m1 != m2 {
+        1
+    } else {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(usize, usize, usize, usize, usize);
+
+    #[test]
+    fn test() {
+        let tests = [
+            TestCase(11, 16, 11, 17, 0),
+            TestCase(11, 30, 12, 1, 1),
+        ];
+
+        for TestCase(m1, d1, m2, d2, expected) in tests {
+            assert_eq!(run(m1, d1, m2, d2), expected);
+        }
+    }
+}
