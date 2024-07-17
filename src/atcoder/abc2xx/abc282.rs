6c5f118c390481268ab823a206671a7ab93c90d2
kento <kento@toriwatari.bird>
2024-07-17 20:48:42 +0900

    🎹 Update

--- src/atcoder/abc2xx/abc282.rs
+++ src/atcoder/abc2xx/abc282.rs
@@ -1,2 +1,3 @@
 pub mod a_generalized_abc;
 pub mod b_lets_get_a_perfect_score;
+pub mod c_string_delimiter;
--- src/atcoder/other_contest/cf16_final/a_wheres_snuke.rs
+++ src/atcoder/other_contest/cf16_final/a_wheres_snuke.rs
@@ -14,3 +14,38 @@ pub fn run(_h: usize, _w: usize, s: Vec<Vec<&str>>) -> (char, usize) {
 
     unreachable!()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(usize, usize, Vec<Vec<&'static str>>, (char, usize));
+
+    #[test]
+    fn test() {
+        let test = [
+            TestCase(15, 10, vec![
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snuke", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+                vec!["snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake", "snake"],
+            ], ('H', 6)),
+            TestCase(1, 1, vec![vec!["snuke"]], ('A', 1)),
+        ];
+
+        for TestCase(h, w, s, expected) in test {
+            assert_eq!(run(h, w, s), expected);
+        }
+    }
+}
