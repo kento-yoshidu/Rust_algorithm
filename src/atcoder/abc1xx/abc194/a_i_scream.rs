1df05484f158e1705ce86d59717cdd8d56b07486
kento <kento@toriwatari.bird>
2024-07-22 20:51:12 +0900

    🍬 Update

--- src/atcoder/abc1xx/abc194/a_i_scream.rs
+++ src/atcoder/abc1xx/abc194/a_i_scream.rs
@@ -16,10 +16,18 @@ pub fn run(a: usize, b: usize) -> usize {
 mod tests {
     use super::*;
 
+    struct TestCase(usize, usize, usize);
+
     #[test]
     fn test() {
-        assert_eq!(1, run(10, 8));
-        assert_eq!(3, run(1, 2));
-        assert_eq!(4, run(0, 0));
+        let tests = [
+            TestCase(10, 8, 1),
+            TestCase(1, 2, 3),
+            TestCase(0, 0, 4),
+        ];
+
+        for TestCase(a, b, expected) in tests {
+            assert_eq!(run(a, b), expected);
+        }
     }
 }
--- src/atcoder/abc3xx/abc363.rs
+++ src/atcoder/abc3xx/abc363.rs
@@ -1,2 +1,3 @@
 pub mod a_piling_up;
 pub mod b_japanese_cursed_doll;
+pub mod c_avoid_k_palindrome_2;
