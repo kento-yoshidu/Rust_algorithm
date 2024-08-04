13573981d9f3d140fca8111458ee353946dbc470
kento <kento@toriwatari.bird>
2024-08-04 21:26:33 +0900

    🌱 Update

--- src/atcoder/abc1xx/abc173/a_payment.rs
+++ src/atcoder/abc1xx/abc173/a_payment.rs
@@ -1,21 +1,25 @@
 // https://atcoder.jp/contests/abc173/tasks/abc173_a
 
-pub fn run(n: i32) -> i32 {
-    if n % 1000 == 0 {
-        return 0;
-    }
-
-    (n as f64 / 1000.0).ceil() as i32 * 1000 - n
+fn run(n: usize) -> usize {
+    (n as f64 / 1000.0).ceil() as usize * 1000 - n
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct TestCase(usize, usize);
+
     #[test]
     fn test() {
-        assert_eq!(100, run(1900));
-        assert_eq!(0, run(3000));
-        assert_eq!(475, run(3525));
+        let tests = [
+            TestCase(1900, 100),
+            TestCase(3000, 0),
+            TestCase(3525, 475),
+        ];
+
+        for TestCase(n, expected) in tests {
+            assert_eq!(run(n), expected);
+        }
     }
 }
