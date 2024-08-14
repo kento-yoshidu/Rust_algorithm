db835a9689cf672d804618e4536b8ab20a56b780
kento <kento@toriwatari.bird>
2024-08-14 13:09:40 +0900

    🆒 Update

--- src/atcoder/abc1xx/abc111/a_atcoder_beginner_contest_999.rs
+++ src/atcoder/abc1xx/abc111/a_atcoder_beginner_contest_999.rs
@@ -1,43 +1,51 @@
 // https://atcoder.jp/contests/abc111/tasks/abc111_a
 
-pub fn run(n: i32) -> i32 {
+fn run(n: usize) -> usize {
     let mut result = String::new();
 
-    n.to_string().chars().for_each(|i| {
-        if i == '9' {
-            result.push('1')
-        } else {
-            result.push('9')
-        }
-    });
+    n.to_string()
+        .chars()
+        .for_each(|i| {
+            if i == '9' {
+                result.push('1')
+            } else {
+                result.push('9')
+            }
+        });
 
-    result.parse::<i32>().unwrap()
+    result.parse::<usize>().unwrap()
 }
 
-fn run2(n: i32) -> i32 {
-    let result = n.to_string().chars().map(|c| {
-        match c {
-            '1' => '9',
-            _ => '1'
-        }
-    }).collect::<String>();
-
-    result.parse::<i32>().unwrap()
+fn run2(n: usize) -> usize {
+    n.to_string()
+        .chars()
+        .map(|c| {
+            match c {
+                '1' => '9',
+                _ => '1'
+            }
+        })
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test() {
-        assert_eq!(991, run(119));
-        assert_eq!(111, run(999));
-    }
+    struct TestCase(usize, usize);
 
     #[test]
-    fn test2() {
-        assert_eq!(991, run2(119));
-        assert_eq!(111, run2(999));
+    fn test() {
+        let tests = [
+            TestCase(119, 991),
+            TestCase(999, 111),
+        ];
+
+        for TestCase(n, expected) in tests {
+            assert_eq!(run(n), expected);
+            assert_eq!(run2(n), expected);
+        }
     }
 }
