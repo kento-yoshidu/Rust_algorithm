d35c4df2969a82695d53ea33fb00ebb713d6dfd6
kento <kento@toriwatari.bird>
2024-08-14 11:17:28 +0900

    🌴 Update

--- src/atcoder/abc1xx/abc186/a_brick.rs
+++ src/atcoder/abc1xx/abc186/a_brick.rs
@@ -1,16 +1,24 @@
 // https://atcoder.jp/contests/abc186/tasks/abc186_a
 
-pub fn run(n: i32, w: i32) -> i32 {
-    (n as f64 / w as f64).floor() as i32
+pub fn run(n: usize, w: usize) -> usize {
+    n  / w
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct TestCase(usize, usize, usize);
+
     #[test]
     fn test() {
-        assert_eq!(3, run(10, 3));
-        assert_eq!(1000, run(1000, 1));
+        let tests = [
+            TestCase(10, 3, 3),
+            TestCase(1000, 1, 1000),
+        ];
+
+        for TestCase(n, w, expected) in tests {
+            assert_eq!(run(n, w), expected);
+        }
     }
 }
