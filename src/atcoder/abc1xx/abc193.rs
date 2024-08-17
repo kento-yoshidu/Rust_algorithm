40ff1aae9bf273ba44e6a16a3f1fa32391aa5aa0
kento <kento@toriwatari.bird>
2024-08-17 12:26:28 +0900

    🔼 Update

--- src/atcoder/abc1xx/abc193.rs
+++ src/atcoder/abc1xx/abc193.rs
@@ -1,2 +1,3 @@
 pub mod a_discount;
 pub mod b_play_snuke;
+pub mod c_unexpressed;
---
+++ src/atcoder/abc1xx/abc193/c_unexpressed.rs
@@ -0,0 +1,43 @@
+// https://atcoder.jp/contests/abc193/tasks/abc193_c
+
+use std::collections::HashSet;
+use num_integer::Roots;
+
+pub fn run(n: usize) -> usize {
+    let mut hash_set: HashSet<usize> = HashSet::new();
+
+    for num in 2..=(n.sqrt()) {
+        let mut tmp = num * num;
+
+        loop {
+            if tmp > n {
+                break;
+            }
+
+            hash_set.insert(tmp);
+            tmp *= num;
+        }
+    }
+
+    n - hash_set.len()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(usize, usize);
+
+    #[test]
+    fn test() {
+        let tests = [
+            TestCase(8, 6),
+            TestCase(100000, 99634),
+        ];
+
+        for TestCase(n, expected) in tests {
+            assert_eq!(run(n), expected);
+        }
+
+    }
+}
