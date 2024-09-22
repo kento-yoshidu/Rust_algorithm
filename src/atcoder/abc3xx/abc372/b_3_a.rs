c7e9f98afad1a8583adcea7e057b3b00b7f82a98
kento <kento@toriwatari.bird>
2024-09-22 12:54:05 +0900

    🙋‍♂️ Update

---
+++ src/atcoder/abc3xx/abc372/b_3_a.rs
@@ -0,0 +1,39 @@
+// https://atcoder.jp/contests/abc372/tasks/abc372_b
+
+fn run(m: isize) -> (usize, Vec<isize>) {
+    let mut rest = m;
+    let mut count = 0;
+    let mut vec = Vec::new();
+
+    for i in (0..=20).rev() {
+        let num = 3_isize.pow(i);
+
+        while num <= rest {
+            count += 1;
+            rest -= num;
+            vec.push(i as isize);
+        }
+    }
+
+    (count, vec)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(isize, (usize, Vec<isize>));
+
+    #[test]
+    fn test() {
+        let tests = [
+            TestCase(6, (2, vec![1, 1])),
+            TestCase(100, (4, vec![4, 2, 2, 0])),
+            TestCase(59048, (20, vec![9, 9, 8, 8, 7, 7, 6, 6, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 0, 0])),
+        ];
+
+        for TestCase(m, expected) in tests {
+            assert_eq!(run(m), expected);
+        }
+    }
+}
