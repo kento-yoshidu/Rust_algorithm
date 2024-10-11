36f44778884311c975f16133307ea4f32e41b0f0
kento <kento@toriwatari.bird>
2024-10-11 13:28:25 +0900

    🧦 Update

--- src/atcoder/abc0xx/abc036/a_tea.rs
+++ src/atcoder/abc0xx/abc036/a_tea.rs
@@ -1,16 +1,29 @@
 // https://atcoder.jp/contests/abc036/tasks/abc036_a
 
-pub fn run(a: usize, b: usize) -> usize {
+fn run(a: usize, b: usize) -> usize {
     (b as f64 / a as f64).ceil() as usize
+}
+
+fn run2(a: usize, b: usize) -> usize {
+    (b + a - 1) / a
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct TestCase(usize, usize, usize);
+
     #[test]
     fn test() {
-        assert_eq!(3, run(12, 36));
-        assert_eq!(9, run(12, 100));
+        let tests = [
+            TestCase(12, 36, 3),
+            TestCase(12, 100, 9),
+        ];
+
+        for TestCase(a, b, expected) in tests {
+            assert_eq!(run(a, b), expected);
+            assert_eq!(run2(a, b), expected);
+        }
     }
 }
