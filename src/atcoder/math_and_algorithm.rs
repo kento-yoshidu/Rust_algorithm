5b780e578660a1db706f81a49c4da3efc9d82386
kento <kento@toriwatari.bird>
2024-10-11 19:47:59 +0900

    🍀 Update

--- src/atcoder/math_and_algorithm.rs
+++ src/atcoder/math_and_algorithm.rs
@@ -1,2 +1,3 @@
 pub mod ma_015_calculate_gcd;
+pub mod ma002_sum_of_3_integers;
 pub mod ma001_print_5_plus_n;
---
+++ src/atcoder/math_and_algorithm/ma002_sum_of_3_integers.rs
@@ -0,0 +1,25 @@
+// https://atcoder.jp/contests/math-and-algorithm/tasks/math_and_algorithm_b
+
+fn run(a1: usize, a2: usize, a3: usize) -> usize {
+    a1 + a2 + a3
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(usize, usize, usize, usize);
+
+    #[test]
+    fn test() {
+        let tests = [
+            TestCase(10, 20, 50, 80),
+            TestCase(1, 1, 1, 3),
+            TestCase(100, 100, 100, 300),
+        ];
+
+        for TestCase(a1, a2, a3, expected) in tests {
+            assert_eq!(run(a1, a2, a3), expected);
+        }
+    }
+}
