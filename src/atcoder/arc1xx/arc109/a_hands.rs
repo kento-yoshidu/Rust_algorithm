e10cc24710216da05541631402ba302d7805488a
kento <kento@toriwatari.bird>
2024-10-18 21:01:40 +0900

    🌱 Update

--- src/atcoder/arc1xx/arc109/a_hands.rs
+++ src/atcoder/arc1xx/arc109/a_hands.rs
@@ -11,3 +11,22 @@ pub fn run(a: isize, b: isize, x: isize, y: isize) -> isize {
         x
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(isize, isize, isize, isize, isize);
+
+    #[test]
+    fn test() {
+        let tests = [
+            TestCase(2, 1, 1, 5, 1),
+            TestCase(1, 2, 100, 1, 101),
+        ];
+
+        for TestCase(a, b, x, y, expected) in tests {
+            assert_eq!(run(a, b, x, y), expected);
+        }
+    }
+}
