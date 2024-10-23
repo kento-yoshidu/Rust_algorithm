0d0d6ce790a18c7579bd4bb4c02655518cf036bd
kento <kento@toriwatari.bird>
2024-10-23 21:15:51 +0900

    🥋 Update

--- src/atcoder/abc0xx/abc001/a_snow_depth.rs
+++ src/atcoder/abc0xx/abc001/a_snow_depth.rs
@@ -1,19 +1,25 @@
 // https://atcoder.jp/contests/abc001/tasks/abc001_1
 
-pub fn run(a: i32, b: i32) -> i32 {
+fn run(a: isize, b: isize) -> isize {
     a - b
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rstest::*;
 
-    #[rstest]
-    #[case::c_1st(15, 10, 5)]
-    #[case::c_2nd(0, 0, 0)]
-    #[case::c_3rd(5, 20, -15)]
-    fn test(#[case] a: i32, #[case] b: i32, #[case] expected: i32) {
-        assert_eq!(expected, run(a, b));
+    struct TestCase(isize, isize, isize);
+
+    #[test]
+    fn test() {
+        let tests = [
+            TestCase(15, 10, 5),
+            TestCase(0, 0, 0),
+            TestCase(5, 20, -15),
+        ];
+
+        for TestCase(a, b, expected) in tests {
+            assert_eq!(run(a, b), expected);
+        }
     }
 }
