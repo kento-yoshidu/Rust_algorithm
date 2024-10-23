4768029d1c8e27c57d92d70008c6271d6a2ff6df
kento <kento@toriwatari.bird>
2024-10-23 21:45:29 +0900

    🚒 Update

--- src/atcoder/abc0xx/abc003/b_playing_cards.rs
+++ src/atcoder/abc0xx/abc003/b_playing_cards.rs
@@ -1,39 +1,49 @@
 // https://atcoder.jp/contests/abc003/tasks/abc003_2
 
-pub fn run(s: String, t: String) -> String {
+fn run(s: &str, t: &str) -> &'static str {
     let arr = ['a', 't', 'c', 'o', 'd', 'e', 'r'];
 
-    if s.chars().zip(t.chars()).all(|t| {
-        if t.0 == t.1 {
-            true
-        } else {
-            if t.0 == '@' && t.1 == '@' {
-                true
-            } else if t.0 != '@' && t.1 != '@' {
-                false
-            } else if t.0 == '@' && arr.contains(&t.1) {
-                true
-            } else if t.1 == '@' && arr.contains(&t.0) {
+    if s.chars()
+        .zip(t.chars())
+        .all(|t| {
+            if t.0 == t.1 {
                 true
             } else {
-                false
+                if t.0 == '@' && t.1 == '@' {
+                    true
+                } else if t.0 != '@' && t.1 != '@' {
+                    false
+                } else if t.0 == '@' && arr.contains(&t.1) {
+                    true
+                } else if t.1 == '@' && arr.contains(&t.0) {
+                    true
+                } else {
+                    false
+                }
             }
+        }) {
+            "You can win"
+        } else {
+            "You will lose"
         }
-    }) {
-        String::from("You can win")
-    } else {
-        String::from("You will lose")
-    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct TestCase(&'static str, &'static str, &'static str);
+
     #[test]
     fn test() {
-        assert_eq!(String::from("You can win"), run(String::from("ch@ku@ai"), String::from("choku@@i")));
-        assert_eq!(String::from("You will lose"), run(String::from("aoki"), String::from("@ok@")));
-        assert_eq!(String::from("You will lose"), run(String::from("arc"), String::from("abc")));
+        let tests = [
+            TestCase("ch@ku@ai", "choku@@i", "You can win"),
+            TestCase("aoki", "@ok@", "You will lose"),
+            TestCase("arc", "abc", "You will lose"),
+        ];
+
+        for TestCase(s, t, expected) in tests {
+            assert_eq!(run(s, t), expected);
+        }
     }
 }
--- src/atcoder/other_contest/joi2025yo1b.rs
+++ src/atcoder/other_contest/joi2025yo1b.rs
@@ -1,2 +1,3 @@
 pub mod a_footrace;
 pub mod b_railway_trip_3;
+pub mod c_rock_scissors_paper;
