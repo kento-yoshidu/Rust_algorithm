6809d2500c303c7ac07ecb4fcf1a45c378598f20
kento <kento@toriwatari.bird>
2024-10-27 20:57:04 +0900

    🏠 Update

--- src/atcoder/abc0xx/abc009/b_family_restaurant.rs
+++ src/atcoder/abc0xx/abc009/b_family_restaurant.rs
@@ -1,18 +1,29 @@
 // https://atcoder.jp/contests/abc009/tasks/abc009_2
 
+use itertools::Itertools;
 
 fn run(_n: usize, a: Vec<usize>) -> usize {
-    let mut vec = a.clone();
+    let vec: Vec<usize> = a.into_iter().sorted().rev().dedup().collect();
 
-    vec.sort();
-    vec.reverse();
-    vec.dedup();
-
-    *vec.iter().nth(1).unwrap()
+    vec.into_iter().nth(1).unwrap()
 }
 
-fn main() {
-    println!("{}", run(4, vec![100, 200, 300, 300]));
-    println!("{}", run(5, vec![50, 370, 819, 433, 120]));
-    println!("{}", run(6, vec![100, 100, 100, 200, 200, 200]));
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(usize, Vec<usize>, usize);
+
+    #[test]
+    fn test() {
+        let tests = [
+            TestCase(4, vec![100, 200, 300, 300], 200),
+            TestCase(5, vec![50, 370, 819, 433, 120], 433),
+            TestCase(6, vec![100, 100, 100, 200, 200, 200], 100),
+        ];
+
+        for TestCase(n, a, expected) in tests {
+            assert_eq!(run(n, a), expected);
+        }
+    }
 }
