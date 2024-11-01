1e89b9099707bd0fdd3280494c7de7468e70bc1b
kento <kento@toriwatari.bird>
2024-11-01 21:01:10 +0900

    🐈 Update

--- src/atcoder/abc0xx/abc012/a_swap.rs
+++ src/atcoder/abc0xx/abc012/a_swap.rs
@@ -1,16 +1,24 @@
 // https://atcoder.jp/contests/abc012/tasks/abc012_1
 
-pub fn run(a: usize, b: usize) -> Vec<usize> {
-    vec![b, a]
+fn run(a: usize, b: usize) -> (usize, usize) {
+    (b, a)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct TestCase(usize, usize, (usize, usize));
+
     #[test]
     fn test() {
-        assert_eq!(vec![2, 1], run(1, 2));
-        assert_eq!(vec![5, 5], run(5, 5));
+        let tests = [
+            TestCase(1, 2, (2, 1)),
+            TestCase(5, 5, (5, 5)),
+        ];
+
+        for TestCase(a, b, expected) in tests {
+            assert_eq!(run(a, b), expected);
+        }
     }
 }
