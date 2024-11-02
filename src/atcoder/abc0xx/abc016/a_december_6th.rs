508dbdde610054d15c4cccc1e75acccb40d67e2f
kento <kento@toriwatari.bird>
2024-11-03 07:55:00 +0900

    🧐 Update

--- src/atcoder/abc0xx/abc016/a_december_6th.rs
+++ src/atcoder/abc0xx/abc016/a_december_6th.rs
@@ -1,10 +1,10 @@
 // https://atcoder.jp/contests/abc016/tasks/abc016_1
 
-pub fn run(m: usize, d: usize) -> String {
+fn run(m: usize, d: usize) -> &'static str {
     if m % d == 0 {
-        String::from("Yes")
+        "Yes"
     } else {
-        String::from("No")
+        "No"
     }
 }
 
@@ -12,10 +12,18 @@ pub fn run(m: usize, d: usize) -> String {
 mod tests {
     use super::*;
 
+    struct TestCase(usize, usize, &'static str);
+
     #[test]
     fn test() {
-        assert_eq!(String::from("Yes"), run(1, 1));
-        assert_eq!(String::from("No"), run(2, 29));
-        assert_eq!(String::from("Yes"), run(12, 6));
+        let tests = [
+            TestCase(1, 1, "Yes"),
+            TestCase(2, 29, "No"),
+            TestCase(12, 6, "Yes"),
+        ];
+
+        for TestCase(m, d, expected) in tests {
+            assert_eq!(run(m, d), expected);
+        }
     }
 }
