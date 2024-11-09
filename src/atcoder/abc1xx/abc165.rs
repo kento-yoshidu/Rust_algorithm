d31f239115ebb6c8e3008fa9e3fa97277a34423c
kento <kento@toriwatari.bird>
2024-11-09 21:07:15 +0900

    🧽 Update

--- src/atcoder/abc1xx/abc165.rs
+++ src/atcoder/abc1xx/abc165.rs
@@ -1,2 +1,3 @@
 pub mod a_we_love_golf;
 pub mod b_one_percent;
+pub mod d_floor_function;
---
+++ src/atcoder/abc1xx/abc165/d_floor_function.rs
@@ -0,0 +1,28 @@
+// https://atcoder.jp/contests/abc165/tasks/abc165_d
+
+use std::cmp::min;
+
+fn run(a: f64, b: f64, n: f64) -> usize {
+    let x = f64::min(b - 1.0, n) as f64;
+
+    ((a*x/b).floor() - a * (x/b).floor()) as usize
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(f64, f64, f64, usize);
+
+    #[test]
+    fn test() {
+        let tests = [
+            TestCase(5.0, 7.0, 4.0, 2),
+            TestCase(11.0, 10.0, 9.0, 9),
+        ];
+
+        for TestCase(a, b, n, expected) in tests {
+            assert_eq!(run(a, b, n), expected);
+        }
+    }
+}
