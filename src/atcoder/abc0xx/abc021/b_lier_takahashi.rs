df3d28eafe802cf752d223a53db69048fb8aa4e5
kento <kento@toriwatari.bird>
2024-11-11 19:56:09 +0900

    🐦 Update

--- src/atcoder/abc0xx/abc021/b_lier_takahashi.rs
+++ src/atcoder/abc0xx/abc021/b_lier_takahashi.rs
@@ -1,6 +1,6 @@
 // https://atcoder.jp/contests/abc021/tasks/abc021_b
 
-pub fn run(_n: usize, a: usize, b: usize, _k: usize, p: Vec<usize>) -> String {
+fn run(_n: usize, a: usize, b: usize, _k: usize, p: Vec<usize>) -> &'static str {
     let mut vec = vec![a, b];
 
     vec.append(&mut p.clone());
@@ -8,9 +8,9 @@ pub fn run(_n: usize, a: usize, b: usize, _k: usize, p: Vec<usize>) -> String {
     vec.dedup();
 
     if vec.len() == p.len() + 2 {
-        String::from("Yes")
+        "Yes"
     } else {
-        String::from("No")
+        "No"
     }
 }
 
@@ -18,12 +18,20 @@ pub fn run(_n: usize, a: usize, b: usize, _k: usize, p: Vec<usize>) -> String {
 mod tests {
     use super::*;
 
+    struct TestCase(usize, usize, usize, usize, Vec<usize>, &'static str);
+
     #[test]
     fn test() {
-        assert_eq!(String::from("Yes"), run(5, 1, 5, 3, vec![3, 4, 2]));
-        assert_eq!(String::from("No"), run(7, 1, 3, 4, vec![2, 4, 2, 7]));
-        assert_eq!(String::from("No"), run(4, 1, 4, 3, vec![2, 1, 3]));
-        assert_eq!(String::from("No"), run(4, 1, 4, 3, vec![2, 4, 3]));
-        assert_eq!(String::from("Yes"), run(20, 1, 4, 12, vec![2, 3, 5, 7, 8, 9, 10, 11, 12, 15, 13, 14]));
+        let tests = [
+            TestCase(5, 1, 5, 3, vec![3, 4, 2], "Yes"),
+            TestCase(7, 1, 3, 4, vec![2, 4, 2, 7], "No"),
+            TestCase(4, 1, 4, 3, vec![2, 1, 3], "No"),
+            TestCase(4, 1, 4, 3, vec![2, 4, 3], "No"),
+            TestCase(20, 1, 4, 12, vec![2, 3, 5, 7, 8, 9, 10, 11, 12, 15, 13, 14], "Yes"),
+        ];
+
+        for TestCase(n, a, b, k, p, expected) in tests {
+            assert_eq!(run(n, a, b, k, p), expected);
+        }
     }
 }
