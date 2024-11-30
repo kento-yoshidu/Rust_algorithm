5d536cb671e762ab4add78e024c4f6b399243c8d
kento <kento@toriwatari.bird>
2024-11-30 14:56:42 +0900

    🗼 ABC116 C

--- src/atcoder/abc0xx/abc029/b_kaki.rs
+++ src/atcoder/abc0xx/abc029/b_kaki.rs
@@ -1,20 +1,29 @@
 // https://atcoder.jp/contests/abc016/tasks/abc016_2
 
-pub fn run(s: Vec<&str>) -> usize {
-    s.iter().filter(|str| {
-        str.chars().any(|c| c == 'r')
-    }).count()
+fn run(s: Vec<&str>) -> usize {
+    s.iter()
+        .filter(|str| {
+            str.chars().any(|c| c == 'r')
+        }).count()
 }
-
-// https://atcoder.jp/contests/abc029/submissions/42272316
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct TestCase(Vec<&'static str>, usize);
+
     #[test]
     fn test() {
-        assert_eq!(8, run(vec!["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]));
-        assert_eq!(11, run(vec!["rrrrrrrrrr", "srrrrrrrrr", "rsr", "ssr", "rrs", "srsrrrrrr", "rssrrrrrr", "sss", "rrr", "srr", "rsrrrrrrrr", "ssrrrrrrrr"]));
+        let tests = [
+            TestCase(vec!["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"], 8),
+            TestCase(vec!["rrrrrrrrrr", "srrrrrrrrr", "rsr", "ssr", "rrs", "srsrrrrrr", "rssrrrrrr", "sss", "rrr", "srr", "rsrrrrrrrr", "ssrrrrrrrr"], 11),
+        ];
+
+        for TestCase(s, expected) in tests {
+            assert_eq!(run(s), expected);
+        }
     }
 }
+
+// https://atcoder.jp/contests/abc029/submissions/42272316
