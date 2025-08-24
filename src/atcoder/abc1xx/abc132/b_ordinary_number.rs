7e01a2e83792d1884db719bfa97de1b92b92a673
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-08-24 20:17:58 +0900

    🪂 Update

--- src/atcoder/abc1xx/abc132/b_ordinary_number.rs
+++ src/atcoder/abc1xx/abc132/b_ordinary_number.rs
@@ -1,6 +1,6 @@
 // https://atcoder.jp/contests/abc132/tasks/abc132_b
 
-pub fn run(_n: usize, p: Vec<usize>) -> usize {
+fn run(_n: usize, p: Vec<usize>) -> usize {
     p.windows(3)
         .filter(|v| {
             (v[0] < v[1] && v[1] < v[2]) || (v[0] > v[1] && v[1] > v[2])
@@ -12,9 +12,17 @@ pub fn run(_n: usize, p: Vec<usize>) -> usize {
 mod tests {
     use super::*;
 
+    struct TestCase(usize, Vec<usize>, usize);
+
     #[test]
-    fn test() {
-        assert_eq!(2, run(5, vec![1, 3, 5, 4, 2]));
-        assert_eq!(5, run(9, vec![9, 6, 3, 2, 5, 8, 7, 4, 1]));
+    fn abc132_b() {
+        let tests = [
+            TestCase(5, vec![1, 3, 5, 4, 2], 2),
+            TestCase(9, vec![9, 6, 3, 2, 5, 8, 7, 4, 1], 5),
+        ];
+
+        for TestCase(n, p, expected) in tests {
+            assert_eq!(run(n, p), expected);
+        }
     }
 }
