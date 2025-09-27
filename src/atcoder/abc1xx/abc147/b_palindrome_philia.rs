ff49857e67dfeb2c403d861cb6709b88edaa388d
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-09-27 13:53:27 +0900

    🦋 Update

--- src/atcoder/abc1xx/abc147/b_palindrome_philia.rs
+++ src/atcoder/abc1xx/abc147/b_palindrome_philia.rs
@@ -1,6 +1,6 @@
 // https://atcoder.jp/contests/abc147/tasks/abc147_b
 
-pub fn run(s: &str) -> usize {
+fn run(s: &str) -> usize {
     let chars: Vec<char> = s.chars().collect();
 
     (0..chars.len()/2)
@@ -14,17 +14,25 @@ pub fn run(s: &str) -> usize {
 mod tests {
     use super::*;
 
+    struct TestCase(&'static str, usize);
+
     #[test]
-    fn test() {
-        assert_eq!(1, run("redcoder"));
-        assert_eq!(0, run("wwwww"));
-        assert_eq!(1, run("rng"));
-        assert_eq!(50, run("ndfzvmkpudjeocebkfpexoszwczmpbdmivjnfeqapwvmbiiiarpwrjyezwdgydqbldyfyslboertiilckvacvroxycczmpfmdymu"));
-        assert_eq!(10, run("aybmyzzankubfabovxfkoazziskrl"));
-        assert_eq!(1, run("ax"));
-        assert_eq!(0, run("xxx"));
-        assert_eq!(34, run("uqoppvgpiqmsiwhpyfqnilmqkokdzowhrkzlavboipnljjlljpjwqalvxfvwpuairhxqiioqflgcwxvjupvghpadng"));
-        assert_eq!(2, run("hjvqwycocvwqvth"));
-        assert_eq!(34, run("xzamzvhfwhndreischtcucykbfjqasqlbkoxjpglbppptrvfccnfvlzppgdlmmseoidlqschqwnkfvqptsriiorvfqdjhrumjfc"));
+    fn abc147_b() {
+        let tests = [
+            TestCase("redcoder", 1),
+            TestCase("wwwww", 0),
+            TestCase("rng", 1),
+            TestCase("ndfzvmkpudjeocebkfpexoszwczmpbdmivjnfeqapwvmbiiiarpwrjyezwdgydqbldyfyslboertiilckvacvroxycczmpfmdymu", 50),
+            TestCase("aybmyzzankubfabovxfkoazziskrl", 10),
+            TestCase("ax", 1),
+            TestCase("xxx", 0),
+            TestCase("uqoppvgpiqmsiwhpyfqnilmqkokdzowhrkzlavboipnljjlljpjwqalvxfvwpuairhxqiioqflgcwxvjupvghpadng", 34),
+            TestCase("hjvqwycocvwqvth", 2),
+            TestCase("xzamzvhfwhndreischtcucykbfjqasqlbkoxjpglbppptrvfccnfvlzppgdlmmseoidlqschqwnkfvqptsriiorvfqdjhrumjfc", 34),
+        ];
+
+        for TestCase(s, expected) in tests {
+            assert_eq!(run(s), expected);
+        }
     }
 }
