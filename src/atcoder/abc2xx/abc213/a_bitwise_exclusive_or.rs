72e1b6d8e9cbe05fa9d6ee84d108b78dfe180056
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-11-16 13:30:30 +0900

    🤢 Update

--- src/atcoder/abc2xx/abc213/a_bitwise_exclusive_or.rs
+++ src/atcoder/abc2xx/abc213/a_bitwise_exclusive_or.rs
@@ -8,9 +8,17 @@ fn run(a: usize, b: usize) -> usize {
 mod tests {
     use super::*;
 
+    struct TestCase(usize, usize, usize);
+
     #[test]
-    fn test() {
-        assert_eq!(5, run(3, 6));
-        assert_eq!(6, run(10, 12));
+    fn abc213_a() {
+        let tests = [
+            TestCase(3, 6, 5),
+            TestCase(10, 12, 6),
+        ];
+
+        for TestCase(a, b, expected) in tests {
+            assert_eq!(run(a, b), expected);
+        }
     }
 }
