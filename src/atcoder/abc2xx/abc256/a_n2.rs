e0a647d73f9c32458da14247150a1b3f89031414
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-12-27 12:28:56 +0900

    🏺 Update

--- src/atcoder/abc2xx/abc256/a_n2.rs
+++ src/atcoder/abc2xx/abc256/a_n2.rs
@@ -1,16 +1,24 @@
 // https://atcoder.jp/contests/abc256/tasks/abc256_a
 
-pub fn run(n: u32) -> i32 {
-    2_i32.pow(n)
+fn run(n: u32) -> u32 {
+    2_u32.pow(n)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    struct TestCase(u32, u32);
+
     #[test]
-    fn test() {
-        assert_eq!(8, run(3));
-        assert_eq!(1073741824, run(30));
+    fn abc256_a() {
+        let tests = [
+            TestCase(3, 8),
+            TestCase(30, 1073741824),
+        ];
+
+        for TestCase(n, expected) in tests {
+            assert_eq!(run(n), expected);
+        }
     }
 }
--- src/atcoder/abc2xx/abc256/b_batters.rs
+++ src/atcoder/abc2xx/abc256/b_batters.rs
@@ -1,6 +1,6 @@
 // https://atcoder.jp/contests/abc256/tasks/abc256_b
 
-pub fn run(n: i32, vec: Vec<i32>) -> i32 {
+fn run(n: usize, a: Vec<usize>) -> usize {
     /* トータルの人数から最後に残っているランナーの人数を引く */
 
     // 最終的に残るランナーの人数
@@ -12,11 +12,11 @@ pub fn run(n: i32, vec: Vec<i32>) -> i32 {
     // vecを後ろから見て行って、累積和が4以上になったらその時点でforループ終了
     // 4以上になった時点でそれまで（配列の前方向）に出ていたランナーは一掃されている
     // 末尾が4の時、ホームランでランナーが一掃されると考えると分かりやすい
-    for i in vec.iter().rev() {
+    for i in a.into_iter().rev() {
         on_base += i;
 
         if on_base >= 4 {
-            break
+            break;
         } else {
             rest_runner += 1;
         }
@@ -29,11 +29,19 @@ pub fn run(n: i32, vec: Vec<i32>) -> i32 {
 mod tests {
     use super::*;
 
+    struct TestCase(usize, Vec<usize>, usize);
+
     #[test]
-    fn test() {
-        assert_eq!(3, run(4, vec![1, 1, 3, 2]));
-        assert_eq!(0, run(3, vec![1, 1, 1]));
-        assert_eq!(8, run(10, vec![2, 2, 4, 1, 1, 1, 4, 2, 2, 1]));
-        assert_eq!(11, run(11, vec![2, 2, 4, 1, 1, 1, 4, 2, 2, 1, 4]));
+    fn abc256_b() {
+        let tests = [
+            TestCase(4, vec![1, 1, 3, 2], 3),
+            TestCase(3, vec![1, 1, 1], 0),
+            TestCase(10, vec![2, 2, 4, 1, 1, 1, 4, 2, 2, 1], 8),
+            TestCase(11, vec![2, 2, 4, 1, 1, 1, 4, 2, 2, 1, 4], 11),
+        ];
+
+        for TestCase(n, a, expected) in tests {
+            assert_eq!(run(n, a), expected);
+        }
     }
 }
