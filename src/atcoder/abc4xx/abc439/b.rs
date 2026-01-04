9a9d2f5171168b8c20c791b7e07dc599286b49ca
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-01-04 13:19:48 +0900

    🍑 ABC439 B

---
+++ src/atcoder/abc4xx/abc439/b.rs
@@ -0,0 +1,49 @@
+// https://atcoder.jp/contests/abc439/tasks/abc439_b
+
+use std::collections::HashSet;
+
+fn rec(n: u32, set: &mut HashSet<u32>) -> &'static str {
+    if n == 1 {
+        "Yes"
+    } else if set.get(&n).is_some() {
+        "No"
+    } else {
+        set.insert(n);
+
+        let str = n.to_string();
+        let i: u32 = str.chars()
+            .map(|c| {
+                let i = c.to_digit(10).unwrap();
+                i * i
+            })
+            .sum();
+
+        rec(i, set)
+    }
+}
+
+fn run(n: u32) -> &'static str {
+    let mut set = HashSet::new();
+
+    rec(n, &mut set)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase(u32, &'static str);
+
+    #[test]
+    fn abc439_b() {
+        let tests = [
+            TestCase(2026, "Yes"),
+            TestCase(439, "No"),
+            TestCase(440, "Yes"),
+        ];
+
+        for TestCase(n, expected) in tests {
+            assert_eq!(run(n), expected);
+        }
+    }
+}
