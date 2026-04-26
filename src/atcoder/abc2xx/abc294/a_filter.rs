d6e8904ff229f16975aca6c8f543e670c241a996
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-04-26 17:56:35 +0900

    ❄️ Update

--- src/atcoder/abc2xx/abc294/a_filter.rs
+++ src/atcoder/abc2xx/abc294/a_filter.rs
@@ -1,8 +1,8 @@
 // https://atcoder.jp/contests/abc294/tasks/abc294_a
 
-pub fn run(_n: usize, vec: Vec<usize>) -> Vec<usize> {
-    vec.into_iter()
-        .filter(|&i| i % 2 == 0)
+fn run(_n: usize, a: Vec<usize>) -> Vec<usize> {
+    a.into_iter()
+        .filter(|i| i % 2 == 0)
         .collect::<Vec<usize>>()
 }
 
@@ -10,10 +10,18 @@ pub fn run(_n: usize, vec: Vec<usize>) -> Vec<usize> {
 mod tests {
     use super::*;
 
+    struct TestCase(usize, Vec<usize>, Vec<usize>);
+
     #[test]
-    fn test() {
-        assert_eq!(vec![2, 6], run(5, vec![1, 2, 3, 5, 6]));
-        assert_eq!(vec![2, 2, 2], run(5, vec![2, 2, 2, 3, 3]));
-        assert_eq!(vec![22, 8, 30, 12, 14], run(10, vec![22, 3, 17, 8, 30, 15, 12, 14, 11, 17]));
+    fn abc294_a() {
+        let tests = [
+            TestCase(5, vec![1, 2, 3, 5, 6], vec![2, 6]),
+            TestCase(5, vec![2, 2, 2, 3, 3], vec![2, 2, 2]),
+            TestCase(10, vec![22, 3, 17, 8, 30, 15, 12, 14, 11, 17], vec![22, 8, 30, 12, 14]),
+        ];
+
+        for TestCase(n, a, expected) in tests {
+            assert_eq!(run(n, a), expected);
+        }
     }
 }
